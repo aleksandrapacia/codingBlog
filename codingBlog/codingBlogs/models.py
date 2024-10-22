292e3fdcfe2755064e9a46fba14e2fb8576a73a7
Aleksandra Pacia <olusiaop05@gmail.com>
2024-10-22 21:32:54 +0200

    activate models

--- codingBlog/codingBlogs/models.py
+++ codingBlog/codingBlogs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+# Place for my models
+class Topic(models.Model):
+    """A topic about coding that user gets to learn about."""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.text
